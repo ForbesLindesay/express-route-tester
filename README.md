ff4c554e7a3ae2e7b3cfd6dddff601ee946295eb
Forbes Lindesay <forbes.lindesay@red-gate.com>
2014-10-31 17:05:34 +0000

    Update and move to npm + browserify and minify JS and add source maps

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 express-route-tester
 ====================
 
-Attempts to give you an idea of what urls will be accepted by an express route (please fork and extend it)
+Attempts to give you an idea of what urls will be accepted by an express route (please fork and extend it).
+
+To build, run:
+
+```
+npm install
+npm run build
+```
--- index.js
+++ index.js
@@ -1,32 +1,56 @@
-$(function () {
-    var pathRegexp = require('path-to-regexp');
-    var debounce = require('debounce');
-    $('#inputRoute, #inputPath').keyup(debounce(update, 200));
-    function update() {
-        var keys = [];
-        var regexp = pathRegexp($('#inputRoute').val(), keys);
-        $('#regexp-display').html(regexp.toString());
-        if (keys.length) {
-            $('#keys-display').html('<ol>' + keys.map(function wrap(key) {
-                return '<li>' + key.name + (key.optional?' (optional)':'') + '</li>';
-            }).join('') + '</ol>');
-        } else {
-            $('#keys-display').html('There are no keys captured by this route');
-        }
+'use strict';
 
-        var path = $('#inputPath').val();
-        if (regexp.test(path)) {
-            $('.is-not-match').hide();
-            $('.is-match').show();
-            var result = regexp.exec(path);
-            $('#keys-results-display').html('<dl class="dl-horizontal">' + keys.map(function (key, i) {
-                return '<dt>' + key.name + '</dt><dd>' + 
-                result[i + 1] + '</dd>';
-            }).join('') + '</dl>');
-        } else {
-            $('.is-not-match').show();
-            $('.is-match').hide();
-        }
-    }
-    update();
-});
+var pathRegexp = require('path-to-regexp');
+var debounce = require('debounce');
+
+var _ = document.querySelector.bind(document);
+
+function escape(str) {
+  return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\"/g, '&quot;');
+}
+function hide(selector) {
+  var elements = document.querySelectorAll(selector);
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.display = 'none';
+  }
+}
+function show(selector) {
+  var elements = document.querySelectorAll(selector);
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.display = null;
+  }
+}
+
+_('#inputRoute').addEventListener('input', debounce(update, 100), false);
+_('#inputPath').addEventListener('input', debounce(updatePath, 100), false);
+
+var keys, regexp;
+function update() {
+  keys = [];
+  regexp = pathRegexp(_('#inputRoute').value, keys);
+  _('#regexp-display').textContent = regexp.toString();
+  if (keys.length) {
+    _('#keys-display').innerHTML = '<ol>' + keys.map(function wrap(key) {
+      return '<li>' + escape(key.name) + (key.optional ? ' (optional)' : '') + '</li>';
+    }).join('') + '</ol>';
+  } else {
+    _('#keys-display').innerHTML = 'There are no keys captured by this route';
+  }
+  updatePath();
+}
+function updatePath() {
+  var path = _('#inputPath').value;
+  if (regexp.test(path)) {
+    hide('.is-not-match');
+    show('.is-match');
+    var result = regexp.exec(path);
+    _('#keys-results-display').innerHTML = '<dl class="dl-horizontal">' + keys.map(function (key, i) {
+      return '<dt>' + escape(key.name) + '</dt><dd>' +
+      escape(result[i + 1]) + '</dd>';
+    }).join('') + '</dl>';
+  } else {
+    show('.is-not-match');
+    hide('.is-match');
+  }
+}
+update();
