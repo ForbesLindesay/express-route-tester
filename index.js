f39c57fa660490e74b387ed67bf8f2b50ee3c27f
Wes Todd <wes@wesleytodd.com>
2018-02-01 10:25:08 -0800

    Multiple versions (#11)
    * standard formatting
    
    * added ability to test multiple versions and options

--- index.js
+++ index.js
@@ -1,56 +1,103 @@
-'use strict';
+'use strict'
 
-var pathRegexp = require('path-to-regexp');
-var debounce = require('debounce');
+var debounce = require('debounce')
 
-var _ = document.querySelector.bind(document);
-
-function escape(str) {
-  return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\"/g, '&quot;');
+var pathRegexp
+var versions = {
+  '0.1.7': require('./versions/0.1.7'),
+  '1.7.0': require('./versions/1.7.0'),
+  '2.0.0': require('./versions/2.0.0')
 }
-function hide(selector) {
-  var elements = document.querySelectorAll(selector);
+
+var _ = document.querySelector.bind(document)
+
+function escape (str) {
+  str = '' + str
+  return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\"/g, '&quot;') // eslint-disable-line no-useless-escape
+}
+function hide (selector) {
+  var elements = document.querySelectorAll(selector)
   for (var i = 0; i < elements.length; i++) {
-    elements[i].style.display = 'none';
+    elements[i].style.display = 'none'
   }
 }
-function show(selector) {
-  var elements = document.querySelectorAll(selector);
+function show (selector) {
+  var elements = document.querySelectorAll(selector)
   for (var i = 0; i < elements.length; i++) {
-    elements[i].style.display = null;
+    elements[i].style.display = null
   }
 }
 
-_('#inputRoute').addEventListener('input', debounce(update, 100), false);
-_('#inputPath').addEventListener('input', debounce(updatePath, 100), false);
+_('#inputStrict').addEventListener('change', updateOptions, false)
+_('#inputSensitive').addEventListener('change', updateOptions, false)
+_('#inputEnd').addEventListener('change', updateOptions, false)
 
-var keys, regexp;
-function update() {
-  keys = [];
-  regexp = pathRegexp(_('#inputRoute').value, keys);
-  _('#regexp-display').textContent = regexp.toString();
+_('#inputVersion').addEventListener('change', setVersion, false)
+_('#inputRoute').addEventListener('input', debounce(update, 100), false)
+_('#inputPath').addEventListener('input', debounce(updatePath, 100), false)
+
+var opts = {
+  strict: false,
+  sensitive: false,
+  end: true
+}
+function updateOptions (e) {
+  switch (e.target.id) {
+    case 'inputStrict':
+      opts.strict = e.target.checked
+      break
+    case 'inputSensitive':
+      opts.sensitive = e.target.checked
+      break
+    case 'inputEnd':
+      opts.end = e.target.checked
+      break
+  }
+  update()
+}
+
+function setVersion () {
+  pathRegexp = versions[_('#inputVersion').value] || pathRegexp
+  update()
+}
+
+var keys, regexp
+function update () {
+  keys = []
+  try {
+    regexp = pathRegexp(_('#inputRoute').value, keys, opts)
+  } catch (e) {
+    show('.is-error')
+    hide('.is-not-match')
+    hide('.is-match')
+    _('#keys-results-display').innerHTML = '<pre><code>' + e.message + '</code></pre>'
+    return
+  }
+  _('#keys-results-display').innerHTML = ''
+  hide('.is-error')
+  _('#regexp-display').textContent = regexp.toString()
   if (keys.length) {
-    _('#keys-display').innerHTML = '<ol>' + keys.map(function wrap(key) {
-      return '<li>' + escape(key.name) + (key.optional ? ' (optional)' : '') + '</li>';
-    }).join('') + '</ol>';
+    _('#keys-display').innerHTML = '<ol>' + keys.map(function wrap (key) {
+      return '<li>' + escape(key.name) + (key.optional ? ' (optional)' : '') + '</li>'
+    }).join('') + '</ol>'
   } else {
-    _('#keys-display').innerHTML = 'There are no keys captured by this route';
+    _('#keys-display').innerHTML = 'There are no keys captured by this route'
   }
-  updatePath();
+  updatePath()
 }
-function updatePath() {
-  var path = _('#inputPath').value;
+function updatePath () {
+  var path = _('#inputPath').value
   if (regexp.test(path)) {
-    hide('.is-not-match');
-    show('.is-match');
-    var result = regexp.exec(path);
+    hide('.is-not-match')
+    show('.is-match')
+    var result = regexp.exec(path)
     _('#keys-results-display').innerHTML = '<dl class="dl-horizontal">' + keys.map(function (key, i) {
       return '<dt>' + escape(key.name) + '</dt><dd>' +
-      (result[i + 1] ? escape(result[i + 1]) : '&nbsp;') + '</dd>';
-    }).join('') + '</dl>';
+      (result[i + 1] ? escape(result[i + 1]) : '&nbsp;') + '</dd>'
+    }).join('') + '</dl>'
   } else {
-    show('.is-not-match');
-    hide('.is-match');
+    show('.is-not-match')
+    hide('.is-match')
   }
 }
-update();
+setVersion()
